e8c1244a26d9cc8a6822a7a1dda8756277959e65
mayank188 <mayanksom36@gmail.com>
2025-01-18 15:55:26 +0530

    addd

---
+++ script.js
@@ -0,0 +1,8 @@
+ const inputbox = document.getElementById("input"); 
+ const list = document.getElementById("list");
+
+ function add(){
+    if(inputbox.value===''){
+        alert("You must write something!")
+    }
+ }
