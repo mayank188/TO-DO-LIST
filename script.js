7863b06436b47a494915df56d2ba3050e82a4387
mayank188 <mayanksom36@gmail.com>
2025-01-20 19:58:37 +0530

    add

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
- const inputbox = document.getElementById("input"); 
- const list = document.getElementById("list");
+const inputbox = document.getElementById("input");
+const listco = document.getElementById("list");
 
- function add(){
-    if(inputbox.value===''){
-        alert("You must write something!")
+function add() {
+    if (inputbox.value === '') {
+        alert("You must write something!");
+    } else {
+        const li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        li.addEventListener("click", function () {
+            li.classList.toggle("check");
+        });
+
+        listco.appendChild(li);
+        inputbox.value = ''; 
     }
- }
+}
