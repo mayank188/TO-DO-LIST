0d89d3775136b21a756ff7f6f8c8dc5be71c48a5
mayank188 <mayanksom36@gmail.com>
2025-01-20 23:57:24 +0530

    complete

--- script.js
+++ script.js
@@ -12,6 +12,30 @@ function add() {
         });
 
         listco.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
         inputbox.value = ''; 
+        saved();
     }
 }
+
+listco.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI"){
+e.target.classList.toggle("checked");
+        saved();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saved();
+    }
+},false);
+
+function saved(){
+    localStorage.setItem("data",listco.innerHTML);
+}
+function show(){
+    listco.innerHTML = localStorage.getItem("data");
+    
+}
+show();
